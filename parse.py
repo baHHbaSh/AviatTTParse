43e4ddcdd6c7a6c1398c81f61c2c2b34a827d574
The_EnG1nE <baHHbaSh@gmail.com>
2024-10-04 18:44:35 +0500

    YaLoh

--- parse.py
+++ parse.py
@@ -1,9 +1,41 @@
+import os, sys, time
+import pandas as pd
+import pycurl
+import tabula
+
+'''
+# Используем tabula для извлечения данных таблиц из PDF
+dfs = tabula.read_pdf(pdf_path, pages='all', lattice=True)
+
+# Объединяем все страницы в один DataFrame (если таблица на нескольких страницах)
+combined_df = pd.concat(dfs, ignore_index=True)
+
+# Сохраняем DataFrame в CSV файл
+combined_df.to_csv(csv_output_path, index=False)
+'''
+
 class Parse:
     def __init__(self) -> None:
-        pass
+        self.PATH_IN = "r.pdf"
+        self.PATH_OUT = "data.csv"
     
-    def Main(self):
+    def ConvertFile(self):
         pass
+    def DownloadFile(self, day):
+        #os.system(f'curl -o "{os.getcwd()}\{self.PATH_IN}" "https://www.permaviat.ru/_engine/get_file.php?f={day}&d=_res/fs/&p=file.pdf"')
+        url = f"https://www.permaviat.ru/_engine/get_file.php?f={day}&d=_res/fs/&p=file.pdf"
+
+        with open(os.path.join(os.getcwd(), self.PATH_IN), 'wb') as file:
+            Curl = pycurl.Curl()
+            Curl.setopt(Curl.URL, url)
+            Curl.setopt(Curl.WRITEDATA, file)
+            Curl.perform()
+            Curl.close()
+
+    def Main(self):
+        self.DownloadFile(3926)
+        time.sleep(1)
+        self.ConvertFile()
 
 if __name__ == "__main__":
-    pass
+    Parse().Main()
